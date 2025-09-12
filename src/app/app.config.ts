66a766b1b9bf1a006a46e9956a6228c871af34eb
uillianhenrique <uillianhenrique@unipam.edu.br>
2025-09-12 16:53:35 -0300

    feat: :art: adicionando uma nova dependencia ng2-charts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,12 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes), provideClientHydration(withEventReplay()), provideCharts(withDefaultRegisterables())
   ]
 };
